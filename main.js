9424b21baa12dfdbc5b6517b6484ceec6d70a4b7
FardinMahadi <mahadihasanfardin2015@gmail.com>
2024-09-06 12:08:58 +0600

    update

---
+++ main.js
@@ -0,0 +1,22 @@
+// function declaration
+function fanOffKor() {
+  console.log("bosa theke uthe dara");
+  console.log("walk towards the switch");
+  console.log("click the switch to off the fan");
+}
+
+// call the function
+fanOffKor();
+
+function doubleIt(number) {
+  const doubled = number * 2;
+  console.log(number, doubled);
+}
+
+console.log("I will call the function");
+doubleIt(15);
+console.log("-------------");
+doubleIt(89);
+console.log("-------------");
+console.log(32);
+console.log("-------------");
